dbc07fd725ed379f076f8fb19bba0666ada327ac
suryalead <100269592+suryalead@users.noreply.github.com>
2022-02-25 08:07:58 +0530

    Add files via upload

---
+++ OddorEven.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package week1;
+
+ 
+public class IdentifyOddOrEven {
+
+
+	public static void main(String[] args) {
+    int input = 10; 
+    if (input % 2 == 0)
+    	System.out.println("Given number is even");
+    else
+    	System.out.println("Given number is odd");
+    
+    	
+
+	}
+
+}
