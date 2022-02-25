f2822c31dcefa05886bd7d1c37c4ead272290df0
suryalead <100269592+suryalead@users.noreply.github.com>
2022-02-25 08:05:35 +0530

    Add files via upload

---
+++ ClassForExecution.java
@@ -0,0 +1,16 @@
+package week1 . day1;
+
+public class ClassForExecution {
+
+          public void sample() {
+        	  System.out.println("Sample Method");
+        	  
+          }
+	public static void main(String[] args) {
+		LearnMethods obj=new LearnMethods();
+		obj.printName();
+		
+
+	}
+
+}
